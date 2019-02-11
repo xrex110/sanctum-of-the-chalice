7bac891d5bb32e7d58541d1ab0e0ed7f689f0df8
Alec Hartline <alechartline8@gmail.com>
2019-02-11 16:45:17 -0500

    Implemented FPS counter for the purposes of benchmarking

--- src/GameView.java
+++ src/GameView.java
@@ -4,7 +4,7 @@ import java.awt.Graphics2D;
 import java.util.Random;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
-
+import java.awt.Font;
 public class GameView extends JPanel {
 	
 	private SpriteLoader loader;
@@ -12,6 +12,11 @@ public class GameView extends JPanel {
 	Random rand;
 	int redraw = 1;
 
+    int frameCount = 0;             /* Tracks number of frames since last check */
+    double timeSinceFPSUpdate = 0;  /* Tracks last time of FPS calculation in MS */
+    double updateDelta = 16;        /* Difference in MS between FPS calculations */
+    double fps = 0;                 /* Number of frames per second */
+    
 	public GameView() {
 		//Fixes a nasty bug that causes repaint() to be called when
 		//the OS requests for it (moving the window offscreen or bringing it
@@ -27,8 +32,12 @@ public class GameView extends JPanel {
 	}
 	@Override
 	public void paint(Graphics g) {
+
+        updateFPS();
 		super.paint(g);		//Clears screen before every paint
 		//SpriteLoader loader = new SpriteLoader();
+        
+
 		Graphics2D rend = (Graphics2D) g;
 
 		int xTiles = 25;
@@ -45,10 +54,27 @@ public class GameView extends JPanel {
 		BufferedImage wizard = loader.getSprite("wizard.png", 0, 32, 32); 
 		rend.drawImage(wizard, null, 320, 320);
 		//System.out.printf("Frame %s took %.2f ms to draw\n",  redraw++, runTime);
+        
+        Font font = new Font("Serif", Font.PLAIN, 14);
+        rend.setFont(font);
+        rend.drawString("Frames Per Second: " + fps,20,20);
 	}
 
 	public void setInputHandler(InputHandler ih) {
 		this.addKeyListener(ih);
 	}
+
+    private void updateFPS() {
+        frameCount++;
+
+        double currentTime = System.nanoTime() / 1e8;
+        if(currentTime - timeSinceFPSUpdate >= updateDelta) {
+            fps = frameCount * 10 / updateDelta;
+            frameCount = 0;
+            timeSinceFPSUpdate = System.nanoTime() / 1e8;
+        }
+
+
+    }
 
 }
