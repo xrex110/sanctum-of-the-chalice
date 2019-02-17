456e10f36faebe266d595f33354baef40469f386
TaehoonKim <lazygame05@gmail.com>
2019-02-17 18:25:27 -0500

    try to loop wav file

--- src/SoundEngine.java
+++ src/SoundEngine.java
@@ -103,37 +103,14 @@ public class SoundEngine {
 
 	
 
-//	public void loop(String fName)
-//	{
+	public void loop(String fName)
+	{
 		
+  		play(fName);
+		al.alSourcei(src[0],AL.AL_LOOPING,1);
+		al.alSourcePlay(src[0]);
 
-//	 	System.out.println("Start Loop");
-	//	static AL al = ALFactory.getAL();
-		//buffer hold sound data
-	//	static int[] buffer = new int[1];
-
-		//source are points emitting sound
-	//	static int[] source = new int[1];
-
-		/*
-		 * Pos and Velocity of the sound source
-		 */
-	///	static float[] sourcePos = { 0.0f, 0.0f, 0.0f};
-	//	static float[] sourceVel = { 0.0f, 0.0f, 0.0f};
-	//	
-		/*
-		 * Position, Velocity, and Orientation of the Listner
-		 * About the Orientation
-		 * 	1st 3 element = "at"
-		 * 	2nd 3 element = "up"
-		 */
-		
-	//	static float[] listnerPos = { 0.0f, 0.0f, 0.0f };
-	//	static float[] listnerVel = { 0.0f, 0.0f, 0.0f };
-	//	static float[] listnerOri = { 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f };
-		
-
-//	}
+	}
 
 
 }
