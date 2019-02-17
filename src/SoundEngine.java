28df2f744e0ab0002a6e30b4e03b34c02d29f8dc
TaehoonKim <lazygame05@gmail.com>
2019-02-17 17:17:39 -0500

    for pull the new conten

--- src/SoundEngine.java
+++ src/SoundEngine.java
@@ -1,9 +1,11 @@
-import com.jogamp.openal.util;
+import com.jogamp.openal.*;
+import com.jogamp.openal.util.*;
 import com.jogamp.openal.AL;
 import com.jogamp.openal.ALException;
 import com.jogamp.openal.ALFactory;
 import com.jogamp.openal.util.ALut;
 
+import java.io.*;
 import java.io.File;
 import java.nio.ByteBuffer;
 
@@ -11,25 +13,46 @@ public class SoundEngine {
 
 	public void play(String fName)
 	{
-		System.out.println("play");
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 	}
 
 	public void loop(String fName)
 	{
+		
 
+	 	System.out.println("Start Loop");
+		static AL al = ALFactory.getAL();
+		//buffer hold sound data
+		static int[] buffer = new int[1];
 
-	 	System.out.println("Hey");
+		//source are points emitting sound
+		static int[] source = new int[1];
 
-
-
-
-
+		/*
+		 * Pos and Velocity of the sound source
+		 */
+		static float[] sourcePos = { 0.0f, 0.0f, 0.0f};
+		static float[] sourceVel = { 0.0f, 0.0f, 0.0f};
+		
+		/*
+		 * Position, Velocity, and Orientation of the Listner
+		 * About the Orientation
+		 * 	1st 3 element = "at"
+		 * 	2nd 3 element = "up"
+		 */
+		
+		static float[] listnerPos = { 0.0f, 0.0f, 0.0f };
+		static float[] listnerVel = { 0.0f, 0.0f, 0.0f };
+		static float[] listnerOri = { 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f };
+		
 
 	}
+
+
 }
