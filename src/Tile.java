2168926630f53e3ac43ef994ce092e3366ae0d2d
Shubham Jain <jainshubhamsunil@gmail.com>
2019-02-20 14:56:25 -0500

    Merge in object engine files

---
+++ src/Tile.java
@@ -0,0 +1,21 @@
+
+import java.awt.Graphics2D;
+
+public class Tile extends GameObject{
+    private final String spriteIdentifier;
+    private final static SpriteLoader spLoader;
+    private final int tileID;
+
+    static {
+        spLoader = new SpriteLoader();
+    }
+
+    public Tile(int x, int y, String spriteIdentifier, int tileID) {
+        super(x, y);
+        this.spriteIdentifier = spriteIdentifier;
+        this.tileID = tileID;
+    }
+    public void draw(Graphics2D rend) {
+        rend.drawImage(spLoader.getSprite(spriteIdentifier, tileID, 32, 32), null, getX(), getY());
+    }
+}
