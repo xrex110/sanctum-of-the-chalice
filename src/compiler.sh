4bfb24cbbab22ff397f321848f06ce7be39db0d7
yang132 <31993113+yang132@users.noreply.github.com>
2019-02-20 19:18:58 -0500

    Add compiler and run srcipt files with classpath

---
+++ src/compiler.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+#run this compile script from the src folder
+javac -cp "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../src;" $1
---
+++ src/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#run this from the src folder
+java -cp "lib/libs/JOAL/jogamp-all-platforms/jar/*;../src" $1
