c6ee0c9c4c9c174dd08b498ee2fa79b69c0a917e
alechartline <alechartline8@gmail.com>
2019-02-23 03:21:31 -0500

    Overhauled menu to use the new Dynamic Buttons

--- src/MenuView.java
+++ src/MenuView.java
@@ -5,26 +5,45 @@ import java.awt.Color;
 public class MenuView extends JPanel {
     private int volume = 5;
     private static int menuSelection = 0;
-    private String[] options;
+    private DynamicButton[] options;
+    private DynamicButton currentSelection;
     private final String menuName;
     private TextDevice menuText;
     private TextDevice titleText;
     public boolean isFocused = false;
     private long lastInputTime = 0;
     private final long INTERACTION_DELAY = 200; //In milliseconds
-    public MenuView(String name) {
+    public MenuView(String name, int width, int height) {
+
         if(name.equals("Main")) isFocused = true;
-        menuName = name;
+
         this.setOpaque(true);
         this.setBackground(Color.black);
-        options = new String[]{
-            "Start",
-                "Null",
-                "Null",
-                "Exit"
-        };
+        this.setSize(width, height);
+
         menuText = new TextDevice("DPComic",20,Color.white, Color.black);
         titleText = new TextDevice("DPComic",40,Color.white, Color.black);
+        menuName = name;
+        
+        Color selectedColor = new Color(0xbb0a1e);
+        Color outline = Color.white;
+        Color fill = new Color(0x002663);
+        
+        int BUTTON_HEIGHT = 50;
+        int BUTTON_WIDTH = 100;
+        int x = (getWidth() - BUTTON_WIDTH) / 2;
+
+        int i = 0;
+        options = new DynamicButton[]{
+            new DynamicButton("Start",x,200 + BUTTON_HEIGHT * 2 * i++,100,BUTTON_HEIGHT,fill,outline,selectedColor,menuText),
+            new DynamicButton("Nothing",x,200 + BUTTON_HEIGHT * 2 * i++,100,BUTTON_HEIGHT,fill,outline,selectedColor,menuText),
+            new DynamicButton("Nothing",x,200 + BUTTON_HEIGHT * 2 * i++,100,BUTTON_HEIGHT,fill,outline,selectedColor,menuText),
+            new DynamicButton("Exit",x,200 + BUTTON_HEIGHT * 2 * i++,100,BUTTON_HEIGHT,fill,outline,selectedColor,menuText),
+        };
+
+        currentSelection = options[0];
+        currentSelection.isSelected = true;
+
     }
 
     public static void setMenuSelection(int id) {
@@ -45,9 +64,15 @@ public class MenuView extends JPanel {
         switch(key) {
             case "W":
                 menuSelection = menuSelection == 0 ? options.length - 1 : menuSelection - 1;
+                currentSelection.isSelected = false;
+                currentSelection = options[menuSelection];
+                currentSelection.isSelected = true;
                 break;
             case "S":
                 menuSelection = menuSelection == options.length-1 ? 0 : menuSelection + 1;
+                currentSelection.isSelected = false;
+                currentSelection = options[menuSelection];
+                currentSelection.isSelected = true;
                 break; 
             case "Enter":
                 if(getMenuSelection() == 0) {
@@ -78,29 +103,10 @@ public class MenuView extends JPanel {
         int titleY = 100;
         titleText.drawOutlineText(rend, "Sanctum of the Chalice", titleX, titleY);
 
-        final int BUTTON_WIDTH = 100;
-        final int BUTTON_HEIGHT = 50;
         for(int i = 0; i < options.length; ++i) {
-            Color fill = new Color(0x002663);
-            if(menuSelection == i)
-                fill = new Color(0xbb0a1e);
-            int buttonX = (getWidth() - BUTTON_WIDTH) / 2;
-            int buttonY = 200 + BUTTON_HEIGHT * 2 * i;
-            Color outline = Color.white;
-
-            drawOutlinedRectangle(rend, outline, fill, buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT);
+            options[i].draw(rend);
             
-            int textX = buttonX + BUTTON_WIDTH/2 - menuText.getPixelWidth(rend, options[i]) / 2;
-            int textY = buttonY + BUTTON_HEIGHT/2 + menuText.getPixelHeight(rend)/4;
-            menuText.drawOutlineText(rend, options[i], textX, textY);
         }
     }
-    public void drawOutlinedRectangle(Graphics2D rend, Color outline, Color fill, int x, int y, int width, int height) {
-        rend.setColor(fill);
-        rend.fillRect(x,y,width,height);
-        rend.setColor(outline);
-        rend.drawRect(x,y,width,height);
-
-    }
-
+    
 }
