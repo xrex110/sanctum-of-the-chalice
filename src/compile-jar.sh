a8be7888528c18e4b93e107ce8622fc5e83a2d52
Shubham Jain <jainshubhamsunil@gmail.com>
2019-03-14 16:57:19 -0400

    Add script to compile jar on linux

---
+++ src/compile-jar.sh
@@ -0,0 +1,5 @@
+rm *.class
+rm Sanctum.jar
+javac -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*:../lib/jsonlib/jackson-core-2.9.8.jar:../lib/jsonlib/jackson-databind-2.9.8.jar:../lib/jsonlib/jackson-annotations-2.9.8.jar:." *.java
+jar -cfvm Sanctum.jar jar-manifest.txt *.class
+rm *.class
