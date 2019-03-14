9a5affce1114d0d68c5fae0f4dfdc34051544f06
Shubham Jain <jain229@purdue.edu>
2019-03-14 17:09:11 -0400

    Add multiple compile and utility files for linux and windows

--- src/compile-jar.sh
+++ src/compile-jar.sh
@@ -1,10 +1,5 @@
-#/bin/bash
-
-#run this compile script from the src folder
-#JAVAFILE="$1"
-#echo -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../src" $JAVAFILE
-
-#rm *.class
-
+rm *.class
+rm Sanctum.jar
 javac -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*:../lib/jsonlib/jackson-core-2.9.8.jar:../lib/jsonlib/jackson-databind-2.9.8.jar:../lib/jsonlib/jackson-annotations-2.9.8.jar:." *.java
-
+jar -cfvm Sanctum.jar jar-manifest.txt *.class
+rm *.class
