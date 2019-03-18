adee8671dcd977aa7d059b8793c579c7fddf8eb3
alechartline <alechartline8@gmail.com>
2019-03-17 23:56:06 -0400

    Added horizontal and vertical gradients as well as density

--- src/Gradient.java
+++ src/Gradient.java
@@ -4,6 +4,7 @@ public class Gradient {
     int x,y,w,h,step;
     boolean isHorizontal;
     int axis, axisWindow;
+    float density = 1.0f;
     Color color;
     public Gradient(int x, int y, int w, int h, boolean horizontal, int axis, int axisWindow, int step, Color color) {
         this.x=x;
@@ -16,13 +17,17 @@ public class Gradient {
         this.step = step;
         this.color = color;
     }
+    public void setDensity(float mult) {
+        density = mult;
+    }
     public void draw(Graphics2D rend) {
+        int GOAL_ALPHA = color.getAlpha();
         for(int i = 0; i < w; i += step) {
-            int alpha = 255;
+            int alpha = GOAL_ALPHA;
             if(i < axis-axisWindow) {
-                alpha = (int)(((float)i/(axis-axisWindow)) * 255);
+                alpha = (int)(((float)i/(axis-axisWindow)) * GOAL_ALPHA);
             } else if(i > axis+axisWindow){
-                alpha = 255 - (int)(((float)(i-axis-axisWindow) / (w-axis-axisWindow)) * 255);
+                alpha = GOAL_ALPHA - (int)(((float)(i-axis-axisWindow) / (w-axis-axisWindow)) * GOAL_ALPHA);
             }
             Color current =  new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
             rend.setColor(current);
@@ -30,16 +35,24 @@ public class Gradient {
         }
     }
     public void drawInverted(Graphics2D rend) {
+        int GOAL_ALPHA = color.getAlpha();
         for(int i = 0; i < w; i += step) {
-            int alpha = 0;
+            float alpha = 0;
             if(i < axis-axisWindow) {
-                alpha = 255-(int)(((float)i/(axis-axisWindow)) * 255);
+                alpha = density * (GOAL_ALPHA-(int)(((float)i/(axis-axisWindow)) * GOAL_ALPHA));
             } else if(i > axis+axisWindow) {
-                alpha = (int)(((float)(i-axis-axisWindow) / (w-axis-axisWindow)) * 255);
+                alpha = density * (int)(((float)(i-axis-axisWindow) / (w-axis-axisWindow)) * GOAL_ALPHA);
             }
-            Color current =  new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+            if(alpha > GOAL_ALPHA) alpha = GOAL_ALPHA;
+            if(alpha < 0) alpha = 0;
+            Color current =  new Color(color.getRed(), color.getGreen(), color.getBlue(), (int)alpha);
             rend.setColor(current);
-            rend.fillRect(i + x, y, step, h);
+            if(!isHorizontal) {
+                rend.fillRect(x, i+y, w, step);
+            }
+            else {
+                rend.fillRect(i + x, y, step, h);
+            }
         }
 
     }
