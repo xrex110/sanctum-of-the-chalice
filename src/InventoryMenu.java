def2f11349ed95be9156386894eb0ed292eac7d3
alechartline <alechartline8@gmail.com>
2019-03-18 02:27:53 -0400

    Added border to inventoryMenu

--- src/InventoryMenu.java
+++ src/InventoryMenu.java
@@ -16,6 +16,15 @@ public class InventoryMenu extends Menu {
 
     final int INVENT_WIDTH = 4;
     final int INVENT_HEIGHT = 7;
+    int inventX = 100;
+    int inventY = 197;
+    int BUTTON_WIDTH = 48;
+    int BUTTON_HEIGHT = 48;
+    int horizGap = 10;
+    int vertGap = 10;
+    int equipmentX = inventX;
+    int equipmentY = inventY - 2*(BUTTON_HEIGHT+vertGap);
+
 
     public InventoryMenu(int width, int height, Menu parent) {
         super(width, height, parent);
@@ -24,12 +33,6 @@ public class InventoryMenu extends Menu {
         sp = new SpriteLoader();
         /* Initialize inventory buttons */
         //Defines the top left corner of the inventory
-        int inventX = 100;
-        int inventY = 197;
-        int BUTTON_WIDTH = 48;
-        int BUTTON_HEIGHT = 48;
-        int horizGap = 10;
-        int vertGap = 10;
 
         for(int i = 0; i < INVENT_WIDTH; ++i) {
             for(int j = 0; j < INVENT_HEIGHT; ++j) {
@@ -42,14 +45,12 @@ public class InventoryMenu extends Menu {
         }
 
         for(DynamicButton b : inventoryButtons) options.add(b);
-        
-        int equipmentX = inventX;
-        int equipmentY = inventY - 2*(BUTTON_HEIGHT+vertGap);
-        for(int i = 0; i < equipmentRender.length; ++i) {
+
+                for(int i = 0; i < equipmentRender.length; ++i) {
             int buttonX = equipmentX + (BUTTON_WIDTH  + horizGap) * i;
             int buttonY = equipmentY;
             equipmentRender[i] =
-                    new DynamicButton(null, buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT, new Color(0x002663), Color.white, new Color(0xbb0a1e));
+                new DynamicButton(null, buttonX, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT, new Color(0x002663), Color.white, new Color(0xbb0a1e));
 
         }
     }
@@ -58,6 +59,9 @@ public class InventoryMenu extends Menu {
             if(!isFocused) return;
             //super.paint(g);
             Graphics2D rend = (Graphics2D) g;
+            
+            drawOutlinedRectangle(rend, Color.white, Color.black, inventX - horizGap, inventY - vertGap,4 * (BUTTON_WIDTH + horizGap) + horizGap, 7*(BUTTON_HEIGHT + vertGap) + vertGap);
+            drawOutlinedRectangle(rend, Color.white, Color.black, equipmentX - horizGap, equipmentY - vertGap,4 * (BUTTON_WIDTH + horizGap) + horizGap, BUTTON_HEIGHT + vertGap + vertGap);
             for(DynamicButton b : inventoryButtons) {
                 b.draw(rend);
             }
@@ -66,6 +70,12 @@ public class InventoryMenu extends Menu {
             }
 
         }
+    public void drawOutlinedRectangle(Graphics2D rend, Color outline, Color fill, int x, int y, int width, int height) {
+        rend.setColor(fill);
+        rend.fillRect(x,y,width,height);
+        rend.setColor(outline);
+        rend.drawRect(x,y,width,height);
+    }
 
     public void invoke(String key) {
         if(!sanitizeInputTime(key)) return;
