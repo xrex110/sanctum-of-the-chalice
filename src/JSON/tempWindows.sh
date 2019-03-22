52700faa9f45bd50a6acd0b2c4e22cf8c7e0b049
yang132 <31993113+yang132@users.noreply.github.com>
2019-03-22 16:39:39 -0400

    Create compile/run script for json on windows

---
+++ src/JSON/tempWindows.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+javac -cp ".;../../lib/jsonlib/jackson-core-2.9.8.jar;../../lib/jsonlib/jackson-databind-2.9.8.jar;../../lib/jsonlib/jackson-annotations-2.9.8.jar" *.java
+echo "Compiled successfully"
+java -cp ".;../../lib/jsonlib/jackson-core-2.9.8.jar;../../lib/jsonlib/jackson-databind-2.9.8.jar;../../lib/jsonlib/jackson-annotations-2.9.8.jar" JsonTest 
+
+rm *.class
