9f807e0aa37295e3ac29fd00cb32226363532cb2
yang132 <31993113+yang132@users.noreply.github.com>
2019-03-22 17:23:25 -0400

    Add separated run and compile scripts for windows

---
+++ src/JSON/windowsRun.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+java -cp ".;../../lib/jsonlib/jackson-core-2.9.8.jar;../../lib/jsonlib/jackson-databind-2.9.8.jar;../../lib/jsonlib/jackson-annotations-2.9.8.jar" JsonTest 
+
+#rm *.class
