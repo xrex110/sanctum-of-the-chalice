3a0ed7816803f5407d40b8b60b94e043dfd93657
Alec Hartline <alechartline8@gmail.com>
2019-03-22 12:43:07 -0400

    Corrected flickering on fire animation

--- src/FireAnimation.java
+++ src/FireAnimation.java
@@ -6,7 +6,12 @@ public class FireAnimation extends Animation {
     }
 
     public BufferedImage getFrame() {
-        return sprites.getSprite(getName(), (getFrameCount()/2) % MAX_FRAME, 100, 100);
+        int f = (getFrameCount() / 2)%MAX_FRAME;
+        
+        if(f == 47 || f == 55 || f == 63) f += 1; 
+
+        BufferedImage sprite = sprites.getSprite(getName(), f%MAX_FRAME, 100, 100);
+        return sprite;
     }
 
 }
