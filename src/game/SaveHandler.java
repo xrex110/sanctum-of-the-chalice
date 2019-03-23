3252846aa831bdeab226188ef7ef7a7e9d9474bf
alechartline <alechartline8@gmail.com>
2019-03-23 14:28:32 -0400

    Moved saving objects to a new thread

--- src/game/SaveHandler.java
+++ src/game/SaveHandler.java
@@ -9,24 +9,29 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class SaveHandler {
-    
+
     /* This class should contain ONLY static saving methods and fields */
     private static final String BIN_PATH = SaveHandler.class.getProtectionDomain().getCodeSource().getLocation().getPath();
     private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SS"); 
     public static void saveScreenshot(BufferedImage image) {
-        try {
-        File directory = new File(BIN_PATH + "screenshots");
-        if(!directory.exists())
-            directory.mkdir();
-        String fileName = formatter.format(new Date()) + ".png";
-        File outputFile = new File(directory.getPath() + "/" + fileName);
-        ImageIO.write(image, "png", outputFile);
-        System.out.println("Saved a screenshot to " + outputFile);
-        //System.out.println(format.format(LocalDate.now()));
-        } catch(Exception e) {
-            System.out.println("Failed to take screenshot.");
-            return;
-        }
+        new Thread(new Runnable() {
+            @Override
+                public void run() {
+                    try {
+                        File directory = new File(BIN_PATH + "screenshots");
+                        if(!directory.exists())
+                            directory.mkdir();
+                        String fileName = formatter.format(new Date()) + ".png";
+                        File outputFile = new File(directory.getPath() + "/" + fileName);
+                        ImageIO.write(image, "png", outputFile);
+                        System.out.println("Saved a screenshot to " + outputFile);
+                        //System.out.println(format.format(LocalDate.now()));
+                    } catch(Exception e) {
+                        System.out.println("Failed to take screenshot.");
+                        return;
+                    }
+                }
+        }).start();
     }
 
 }
