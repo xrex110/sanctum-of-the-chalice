6285d6d8c34dfd65c3b0a5fa68777cb95753f7a1
alechartline <alechartline8@gmail.com>
2019-03-23 14:33:00 -0400

    Made compiler preserve saves and screenshots

--- src/linux_run.sh
+++ src/linux_run.sh
@@ -4,9 +4,9 @@
 #JAVAFILE="$1"
 #echo -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../src" $JAVAFILE
 
-rm -rf ../bin/*
+rm -rf ../bin/*/*.class
 
 javac -d ../bin/ -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*:../lib/jsonlib/jackson-core-2.9.8.jar:../lib/jsonlib/jackson-databind-2.9.8.jar:../lib/jsonlib/jackson-annotations-2.9.8.jar:." ./main/Sanctum.java
 java -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*:../lib/jsonlib/jackson-core-2.9.8.jar:../lib/jsonlib/jackson-databind-2.9.8.jar:../lib/jsonlib/jackson-annotations-2.9.8.jar:../bin/" main.Sanctum
 
-rm -rf ../bin/*
+rm -rf ../bin/*/*.class
