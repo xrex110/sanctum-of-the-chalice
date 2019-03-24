06fbd67a90dc055df81906c05b5dc7769f615f84
yang132 <31993113+yang132@users.noreply.github.com>
2019-03-24 01:52:09 -0400

    Update gitbash run script

--- src/gitbash_run.sh
+++ src/gitbash_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-javac -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;." *.java
-java -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;." Sanctum
-rm *.class
+#javac -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;." *.java
+#java -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;." Sanctum
+#rm *.class
+
+rm -rf ../bin/*/*.class
+
+javac -d ../bin/ -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;." ./main/Sanctum.java
+java -classpath "../lib/libs/JOAL/jogamp-all-platforms/jar/*;../lib/jsonlib/jackson-core-2.9.8.jar;../lib/jsonlib/jackson-databind-2.9.8.jar;../lib/jsonlib/jackson-annotations-2.9.8.jar;../bin/" main.Sanctum
+
+rm -rf ../bin/*/*.class
