6a03d2135a2a6a4710e21196855727a82991b530
Alec Hartline <alechartline8@gmail.com>
2019-04-12 17:29:46 -0400

    Add the ability to save the game

--- src/render/SaveView.java
+++ src/render/SaveView.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class SaveView extends Menu {
     DynamicButton saveButton, loadButton, backButton;
     TextDevice menuText = new TextDevice("DPComic",20,Color.white, Color.black);
-    private FadingText savedNotification;
+    private FadingText savedNotification, loadNotification;
     public SaveView(int w, int h, Menu parent) {
         super(w,h,parent);
 
@@ -26,6 +26,7 @@ public class SaveView extends Menu {
         
         TextDevice notifText = new TextDevice("DPComic",20,Color.white, Color.black);
         savedNotification = new FadingText("Saved game!", 50, 50,500,true,notifText);
+        loadNotification = new FadingText("Loaded game!", 50, 50,500,true,notifText);
 
     }
 
@@ -35,19 +36,29 @@ public class SaveView extends Menu {
         switch(key) {
         case "Enter":
             if(selection == 0) {
-                //Call a load pls
-                System.out.println("SAVE PLS");
-                savedNotification.start();
-            }else if(selection == 1) {
                 //Call a save pls
+                savedNotification.start();
+                loadNotification.stop();
+            }else if(selection == 1) {
+                //Call a load pls
+                savedNotification.stop();
+                loadNotification.start();
             }else if(selection == 2) {
                 parent.focus(this);
             }
+            break;
+        case "W":
+            selectButton(selection - 1 >= 0 ? selection-1 : options.size()-1);
+            break;
+        case "S":
+            selectButton(selection + 1 < options.size() ? selection+1 : 0);
+            break;
         }
     }
 
     @Override
     public void paint(Graphics g) {
+
         if(!isFocused) return;
         super.paint(g);
         Graphics2D rend = (Graphics2D) g;
@@ -55,6 +66,7 @@ public class SaveView extends Menu {
         loadButton.draw(rend);
         backButton.draw(rend);
         savedNotification.fadeIn(rend);
+        loadNotification.fadeIn(rend);
         
     }
 
